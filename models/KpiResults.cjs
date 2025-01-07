a6d6489d4b64fb028a84e9227a0ed280cb8a0ebc
dsrodrigovieira <dsrodrigovieira@gmail.com>
2025-01-07 00:21:40 -0300

    ajustado schema KpiResults com objetos

--- models/KpiResults.cjs
+++ models/KpiResults.cjs
@@ -5,77 +5,126 @@ const KpiResultsSchema = new mongoose.Schema({
   year: { type: Number, required: true },
   month: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
-  rkpi1: { type: Number, required: true, default: null },
-  rkpi2_clinico: { type: Number, required: true, default: null },
-  rkpi2_cirurgico: { type: Number, required: true, default: null },
-  rkpi2_geral: { type: Number, required: true, default: null },
-  rkpi3: { type: Number, required: true, default: null },
-  rkpi4_cli_neo_precoce: { type: Number, required: true, default: null },
-  rkpi4_cli_neo_tardio: { type: Number, required: true, default: null },
-  rkpi4_cli_pedi: { type: Number, required: true, default: null },
-  rkpi4_cli_ad: { type: Number, required: true, default: null },
-  rkpi4_cli_idoso: { type: Number, required: true, default: null },
-  rkpi4_cir_neo_precoce: { type: Number, required: true, default: null },
-  rkpi4_cir_neo_tardio: { type: Number, required: true, default: null },
-  rkpi4_cir_pedi: { type: Number, required: true, default: null },
-  rkpi4_cir_ad: { type: Number, required: true, default: null },
-  rkpi4_cir_idoso: { type: Number, required: true, default: null },
-  rkpi4_clinico: { type: Number, required: true, default: null },
-  rkpi4_cirurgico: { type: Number, required: true, default: null },
-  rkpi4_neo_precoce: { type: Number, required: true, default: null },
-  rkpi4_neo_tardio: { type: Number, required: true, default: null },
-  rkpi4_pedi: { type: Number, required: true, default: null },
-  rkpi4_ad: { type: Number, required: true, default: null },
-  rkpi4_idoso: { type: Number, required: true, default: null },
-  rkpi4_geral: { type: Number, required: true, default: null },
-  rkpi5_cli_pedi: { type: Number, required: true, default: null },
-  rkpi5_cli_ad: { type: Number, required: true, default: null },
-  rkpi5_cli_idoso: { type: Number, required: true, default: null },
-  rkpi5_cir_pedi: { type: Number, required: true, default: null },
-  rkpi5_cir_ad: { type: Number, required: true, default: null },
-  rkpi5_cir_idoso: { type: Number, required: true, default: null },
-  rkpi5_clinico: { type: Number, required: true, default: null },
-  rkpi5_cirurgico: { type: Number, required: true, default: null },
-  rkpi5_pedi: { type: Number, required: true, default: null },
-  rkpi5_ad: { type: Number, required: true, default: null },
-  rkpi5_idoso: { type: Number, required: true, default: null },
-  rkpi5_geral: { type: Number, required: true, default: null },
-  rkpi6: { type: Number, required: true, default: null },
-  rkpi7_nvl2: { type: Number, required: true, default: null },
-  rkpi7_nvl3: { type: Number, required: true, default: null },
-  rkpi7_geral: { type: Number, required: true, default: null },
-  rkpi8: { type: Number, required: true, default: null },
-  rkpi9: { type: Number, required: true, default: null },
-  rkpi10_ui_neo: { type: Number, required: true, default: null },
-  rkpi10_ui_pedi: { type: Number, required: true, default: null },
-  rkpi10_ui_ad: { type: Number, required: true, default: null },
-  rkpi10_uti_neo: { type: Number, required: true, default: null },
-  rkpi10_uti_pedi: { type: Number, required: true, default: null },
-  rkpi10_uti_ad: { type: Number, required: true, default: null },
-  rkpi10_neo: { type: Number, required: true, default: null },
-  rkpi10_pedi: { type: Number, required: true, default: null },
-  rkpi10_ad: { type: Number, required: true, default: null },
-  rkpi10_ui: { type: Number, required: true, default: null },
-  rkpi10_uti: { type: Number, required: true, default: null },
-  rkpi10_geral: { type: Number, required: true, default: null },
-  rkpi11_ui_neo: { type: Number, required: true, default: null },
-  rkpi11_ui_pedi: { type: Number, required: true, default: null },
-  rkpi11_ui_ad: { type: Number, required: true, default: null },
-  rkpi11_uti_neo: { type: Number, required: true, default: null },
-  rkpi11_uti_pedi: { type: Number, required: true, default: null },
-  rkpi11_uti_ad: { type: Number, required: true, default: null },
-  rkpi11_neo: { type: Number, required: true, default: null },
-  rkpi11_pedi: { type: Number, required: true, default: null },
-  rkpi11_ad: { type: Number, required: true, default: null },
-  rkpi11_ui: { type: Number, required: true, default: null },
-  rkpi11_uti: { type: Number, required: true, default: null },
-  rkpi11_geral: { type: Number, required: true, default: null },
-  rkpi12_cir_orto: { type: Number, required: true, default: null },
-  rkpi12_cir_n_orto: { type: Number, required: true, default: null },
-  rkpi12_cirurgico: { type: Number, required: true, default: null },
-  rkpi12_geral: { type: Number, required: true, default: null },
-  rkpi13: { type: Number, required: true, default: null },
-  rkpi14: { type: Number, required: true, default: null }
+  rkpi_1: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  },
+  rkpi_2: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "clinico", value: { type: Number } },
+      { type: "cirurgico", value: { type: Number } }
+    ]
+  },
+  rkpi_3: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  },
+  rkpi_4: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "cli_neo_precoce", value: { type: Number } },
+      { type: "cli_neo_tardio", value: { type: Number } },
+      { type: "cli_pedi", value: { type: Number } },
+      { type: "cli_ad", value: { type: Number } },
+      { type: "cli_idoso", value: { type: Number } },
+      { type: "cir_neo_precoce", value: { type: Number } },
+      { type: "cir_neo_tardio", value: { type: Number } },
+      { type: "cir_pedi", value: { type: Number } },
+      { type: "cir_ad", value: { type: Number } },
+      { type: "cir_idoso", value: { type: Number } },
+      { type: "clinico", value: { type: Number } },
+      { type: "cirurgico", value: { type: Number } },
+      { type: "neo_precoce", value: { type: Number } },
+      { type: "neo_tardio", value: { type: Number } },
+      { type: "pedi", value: { type: Number } },
+      { type: "ad", value: { type: Number } },
+      { type: "idoso", value: { type: Number } }
+    ]
+  },
+  rkpi_5: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "cli_pedi", value: { type: Number } },
+      { type: "cli_ad", value: { type: Number } },
+      { type: "cli_idoso", value: { type: Number } },
+      { type: "cir_pedi", value: { type: Number } },
+      { type: "cir_ad", value: { type: Number } },
+      { type: "cir_idoso", value: { type: Number } },
+      { type: "clinico", value: { type: Number } },
+      { type: "cirurgico", value: { type: Number } },
+      { type: "pedi", value: { type: Number } },
+      { type: "ad", value: { type: Number } },
+      { type: "idoso", value: { type: Number } }
+    ]
+  },
+  rkpi_6: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  },
+  rkpi_7: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "nvl2", value: { type: Number } },
+      { type: "nvl3", value: { type: Number } }
+    ]
+  },
+  rkpi_8: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  },
+  rkpi_9: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  },
+  rkpi_10: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "ui_neo", value: { type: Number } },
+      { type: "ui_pedi", value: { type: Number } },
+      { type: "ui_ad", value: { type: Number } },
+      { type: "uti_neo", value: { type: Number } },
+      { type: "uti_pedi", value: { type: Number } },
+      { type: "uti_ad", value: { type: Number } },
+      { type: "neo", value: { type: Number } },
+      { type: "pedi", value: { type: Number } },
+      { type: "ad", value: { type: Number } },
+      { type: "ui", value: { type: Number } },
+      { type: "uti", value: { type: Number } }
+    ]
+  },
+  rkpi_11: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "ui_neo", value: { type: Number } },
+      { type: "ui_pedi", value: { type: Number } },
+      { type: "ui_ad", value: { type: Number } },
+      { type: "uti_neo", value: { type: Number } },
+      { type: "uti_pedi", value: { type: Number } },
+      { type: "uti_ad", value: { type: Number } },
+      { type: "neo", value: { type: Number } },
+      { type: "pedi", value: { type: Number } },
+      { type: "ad", value: { type: Number } },
+      { type: "ui", value: { type: Number } },
+      { type: "uti", value: { type: Number } }
+    ]
+  },
+  rkpi_12: {
+    value: { type: Number, required: true, default: null },
+    estratification: [
+      { type: "cir_orto", value: { type: Number } },
+      { type: "cir_n_orto", value: { type: Number } },
+      { type: "cirurgico", value: { type: Number } }
+    ]
+  },
+  rkpi_13: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  },
+  rkpi_14: { 
+    value: { type: Number, required: true, default: null },
+    estratification: []
+  }
 });
 
 module.exports = mongoose.model("kpi_results", KpiResultsSchema);
