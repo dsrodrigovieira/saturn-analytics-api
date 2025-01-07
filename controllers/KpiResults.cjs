c78e330212fb93d86889bd91d5d1edbbd64a84db
dsrodrigovieira <dsrodrigovieira@gmail.com>
2025-01-06 23:51:29 -0300

    endpoint alterado de post pra get, removido body e incluido parametros na url

--- controllers/KpiResults.cjs
+++ controllers/KpiResults.cjs
@@ -1,9 +1,12 @@
 const KpiResults = require("../models/KpiResults.cjs");
 
 async function getKpiResults(req,res) {
-    const { organization_cnes, month, year } = req.body;
+    const organization_cnes = req.params.cnes;
+    const month = req.params.month;
+    const year = req.params.year;
+
     if (!organization_cnes && !month && !year) {
-        return res.status(400).json({ message: "Informe empresa, ano e competência." }); 
+        return res.status(400).json({ message: "Informe a empresa, o ano e a competência." }); 
     }
     try {
         const kpis = await KpiResults.findOne({ $and: [ { organization_cnes:organization_cnes }, { year:year } , { month:month } ] }, {"_id":0})
