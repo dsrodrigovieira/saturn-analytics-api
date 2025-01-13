9512c0031875517d68533817f1910eb98aa7baaf
Rodrigo Vieira <dsrodrigovieira@gmail.com>
2025-01-13 07:56:13 -0300

    comentários no código

--- server.cjs
+++ server.cjs
@@ -1,19 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const {default: routes} = require("./src/routes/routes.cjs");
+const { default: routes } = require("./src/routes/routes.cjs");
 
+// Carrega as variáveis de ambiente
 dotenv.config();
+
+// Define a porta padrão para o servidor
 const PORT = process.env.PORT || 3000;
+
+// Inicializa o Express
 const app = express();
+
+// Configura as rotas para a aplicação
 routes(app);
 
-// Conectar ao MongoDB
+// Conecta ao banco de dados MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Conectado ao MongoDB"))
-  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
-// Inicializar o servidor
+  .then(() => console.log("Conectado ao MongoDB")) // Loga sucesso na conexão
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err)); // Loga erros na conexão
+
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
