085d2bfdc65754c7982a49efd68b9643f714d490
Rodrigo Vieira <dsrodrigovieira@gmail.com>
2025-01-13 08:08:43 -0300

    comentários no código

--- src/middleware/authToken.cjs
+++ src/middleware/authToken.cjs
@@ -1,18 +1,29 @@
 const jwt = require("jsonwebtoken");
+
 require('dotenv').config();
 
+// Função middleware para autenticar o token presente nos cookies
 function authenticateToken(req, res, next) {
-    const token = req.cookies.authToken; // Recupera o token do cookie  
-    if (!token) {
-      return res.status(401).json({ message: 'Acesso negado. Token ausente.' });
-    }  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded; // Anexa os dados do usuário ao objeto `req` para uso posterior
-      next(); // Continua para o próximo middleware ou rota
-    } catch (error) {
-      return res.status(403).json({ message: 'Token inválido.' });
-    }
-  }
+    // Recupera o token JWT armazenado no cookie 'authToken'
+    const token = req.cookies.authToken;
 
-module.exports = {default:authenticateToken};
+    if (!token) {
+        // Retorna um erro 401 (não autorizado) caso o token esteja ausente
+        return res.status(401).json({ message: 'Acesso negado. Token ausente.' });
+    }
+
+    try {
+        // Tenta verificar e decodificar o token usando
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Adiciona as informações decodificadas do token ao objeto `req`, permitindo acesso em outras partes da aplicação
+        req.user = decoded;        
+        next();
+    } catch (error) {
+        // Retorna um erro 403 (proibido) caso o token seja inválido
+        return res.status(403).json({ message: 'Token inválido.' });
+    }
+}
+
+// Exporta a função como padrão do módulo
+module.exports = { default: authenticateToken };
